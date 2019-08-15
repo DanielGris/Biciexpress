eebfcf6473dea459e84853e2a4c72aa4bdaa705e
DanielGris <54144263+DanielGris@users.noreply.github.com>
2019-08-15 11:53:24 -0500

    Initial commit

---
+++ Biciexpress/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Biciexpress.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Biciexpress.Areas.Identity.IdentityHostingStartup))]
+namespace Biciexpress.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
